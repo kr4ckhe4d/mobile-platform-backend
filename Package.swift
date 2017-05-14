d5c2f5275518ef1e4be46d695726af2b2572e900
Nipuna <nipuna@app-monkeyz.com>
2017-05-14 07:50:48 +0530

    Add MySQL Package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,8 @@ import PackageDescription
 let package = Package(
     name: "mobile-platform-backend",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
+        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 5)
     ],
     exclude: [
         "Config",

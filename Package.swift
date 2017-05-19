d8fd3d15f8e8c936b8aa79378f64346bbb73ffa5
Dihara Wijetunga <dihara.w@fidenz.com>
2017-05-19 10:52:34 +0530

    Added SQLite package. Updated gitgnore.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "mobile-platform-backend",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
-        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 5)
+        .Package(url: "https://github.com/vapor/sqlite-provider.git", majorVersion: 1, minor: 1)
     ],
     exclude: [
         "Config",
